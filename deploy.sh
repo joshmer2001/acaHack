ba98e312b7d9a98ac4ab4572a012ba5707de1224
freesmoke24 <joshmer@hotmail.co.uk>
2023-12-04 12:12:24 +0000

    Initial Commit

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 # *nix only
 export RG="reddog"
 export LOCATION="eastus2"
-export SUB_ID="<YourSubscriptionID>"
+export SUB_ID="e87b7673-b33d-4345-ad10-dc4b91ad1701"
 
 # Follow Azure CLI prompts to authenticate to your subscription of choice
 az login
 az account set --subscription $SUB_ID
 
 # Create resource group
-az group create -n $RG -l $LOCATION
+# az group create -n $RG -l $LOCATION
 
 # Deploy all infrastructure and reddog apps
 az deployment group create -n reddog -g $RG -f ./deploy/bicep/main.bicep
